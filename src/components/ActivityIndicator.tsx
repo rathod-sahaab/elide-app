437bb1fdc85876c4d32fd4acd443a81cb6278138
Abhay <abhayonlyone@gmail.com>
2021-01-30 21:20:02 +0530

    Profile updates

---
+++ src/components/ActivityIndicator.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@chakra-ui/core'
+import * as React from 'react'
+
+export default function ActivityIndicator({ isActive, ...props }) {
+   return (
+      <Button
+         {...props}
+         size="xs"
+         textTransform="uppercase"
+         variantColor={isActive ? 'green' : 'red'}
+      >
+         {isActive ? 'active' : 'inactive'}
+      </Button>
+   )
+}
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,13 +1,46 @@
+import { Box, Button, Flex, Heading } from '@chakra-ui/core'
 import React from 'react'
+import { getUser } from '../services/auth'
+import User from '../models/data/User'
+import ActivityIndicator from './ActivityIndicator'
 
-const Profile = () => (
-   <>
-      <h1>Your profile</h1>
-      <ul>
-         <li>Name: Your name will appear here</li>
-         <li>E-mail: And here goes the mail</li>
-      </ul>
-   </>
-)
+const Profile = () => {
+   let user: User = getUser()
+   return (
+      <Flex width="full" align="center" justifyContent="center">
+         <Box
+            borderWidth={1}
+            p={8}
+            maxWidth="500px"
+            borderRadius={8}
+            position="relative"
+         >
+            <Heading>Hi! {user.name.split(' ')[0]}</Heading>
+            <ActivityIndicator
+               position="absolute"
+               top="4"
+               right="4"
+               isActive={user.active}
+               pointerEvents="none"
+            />
+            <Box p={8} maxWidth="500px" borderRadius={8}>
+               <Heading size="md">Name</Heading>
+               <p>{user.name}</p>
+               <Heading size="md">Email</Heading>
+               <p>{user.email}</p>
+               <Heading size="md">Username</Heading>
+               <p>{user.username}</p>
+               <Button
+                  size="xs"
+                  textTransform="uppercase"
+                  variantColor={user.email_verified ? 'green' : 'red'}
+               >
+                  {user.email_verified ? 'email verified' : 'email unverified'}
+               </Button>
+            </Box>
+         </Box>
+      </Flex>
+   )
+}
 
 export default Profile
