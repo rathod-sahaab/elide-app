0e31d6448d7800759ba6e12ac95b982defd72552
Abhay Raj Singh <abhayonlyone@gmail.com>
2022-07-24 15:43:24 +0530

    Simplified Links components

--- src/features/links/Links.tsx
+++ src/features/links/Links.tsx
@@ -1,29 +1,32 @@
 import { IoMdAdd } from 'react-icons/io'
 import { useSelector } from 'react-redux'
 import { AddLinkCard } from './AddLink'
-import { selectLinks } from './linksSlice'
+import { ILink, selectLinks } from './linksSlice'
+
+const LinkCard = ({ slug, url, active }: ILink) => {
+	const ActivityIndicator = ({ active }: { active: boolean }) => {
+		return (
+			<div className="tooltip tooltip-left" data-tip={active ? 'active' : 'inactive'}>
+				<span className={'badge ' + (active ? 'badge-success' : 'badge-warning')}></span>
+			</div>
+		)
+	}
+
+	return (
+		<div className="card bg-base-200 max-w-sm w-full shadow p-4 [&>*:not(:last-child)]:mb-2">
+			<div className="flex items-center justify-between">
+				<h3 className="font-bold text-accent">{slug}</h3>
+				<ActivityIndicator active={active} />
+			</div>
+			<a href={url} className="link">
+				{url}
+			</a>
+		</div>
+	)
+}
 
 export const Links = () => {
 	const links = useSelector(selectLinks)
-	console.log({ links })
-
-	const linksList = links.map(({ id, slug, url, active }) => {
-		console.log(active)
-		return (
-			<div
-				key={id}
-				className="card bg-base-200 max-w-sm w-full shadow p-4 [&>*:not(:last-child)]:mb-2"
-			>
-				<div className="flex items-center justify-between">
-					<h3 className="font-bold text-accent">{slug}</h3>
-					<span className={'badge ' + (active ? 'badge-success' : 'badge-warning')}></span>
-				</div>
-				<a href={url} className="link">
-					{url}
-				</a>
-			</div>
-		)
-	})
 
 	return (
 		<div className="[&>*:not(:last-child)]:mb-4 pr-6 border-r-2 border-base-300 max-w-sm">
@@ -33,8 +36,10 @@ export const Links = () => {
 					<IoMdAdd size="1.5em" />
 				</button>
 			</div>
+			{links.map((data) => (
+				<LinkCard key={data.id} {...data} />
+			))}
 			<AddLinkCard />
-			{linksList}
 		</div>
 	)
 }
